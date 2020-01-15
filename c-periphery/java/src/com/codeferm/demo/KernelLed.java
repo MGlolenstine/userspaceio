a1aa2789d3756f7f2ea20527f7c6a50735db9cb8
sgoldsmith <sgoldsmith@dx4350>
2020-01-14 20:25:26 -0500

    Test flash

--- c-periphery/java/src/com/codeferm/demo/KernelLed.java
+++ c-periphery/java/src/com/codeferm/demo/KernelLed.java
@@ -5,8 +5,13 @@ import com.sun.jna.ptr.PointerByReference;
 import peripheryled.PeripheryledLibrary;
 
 /**
+ * c-periphery LED wrapper functions for Linux userspace sysfs LEDs. To get LED
+ * names use: 
  * 
- * Copyright (c) 2018 Steven P. Goldsmith See LICENSE.md for details.
+ * cd /sys/class/leds
+ * ls -1F
+ * 
+ * Copyright (c) 2020 Steven P. Goldsmith See LICENSE.md for details.
  */
 
 public class KernelLed {
@@ -16,7 +21,18 @@ public class KernelLed {
 		final PeripheryledLibrary lib = PeripheryledLibrary.INSTANCE;
 		final PointerByReference handle = lib.led_new();
 		int rc = lib.led_open(handle, "nanopi:green:pwr");
-		System.out.println(rc);
+		// Turn on LED full brightness
+		lib.led_write(handle, (byte) 1);
+		for (int i = 0; i < 10; i++) {
+			// LED on
+			System.out.println("\nLED on");
+			lib.led_set_brightness(handle, 1);
+			TimeUnit.SECONDS.sleep(1);
+			// LED off
+			lib.led_set_brightness(handle, 0);
+			System.out.println("LED off");
+			TimeUnit.SECONDS.sleep(1);
+		}
 		lib.led_close(handle);
 		lib.led_free(handle);
 	}
